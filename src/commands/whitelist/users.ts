4f230ec697b596ab15ae9904138ce50d413743d1
Caden-kun <cadenripepi012@gmail.com>
2021-08-02 03:01:11 +0200

    added usercount

---
+++ src/commands/whitelist/users.ts
@@ -0,0 +1,45 @@
+import * as commando from "discord.js-commando";
+import { Message, MessageEmbed } from "discord.js";
+import { CONFIG } from "../../utils/globals";
+export default class MemberCountCommand extends commando.Command {
+    public constructor(client: commando.CommandoClient) {
+        super(client, {
+
+            aliases: ["membercount", "users", "totalusers"],
+
+            description: "shows how many members the bot is serving!",
+
+            group: "utility",
+
+            guildOnly: true,
+
+
+            memberName: "members",
+
+            name: "members",
+
+            ownerOnly: false,
+
+            throttling: {
+                duration: 60,
+                usages: 1
+            },
+            userPermissions: ["SEND_MESSAGES"]
+
+        });
+    }
+
+    public async run(
+        msg: commando.CommandoMessage
+    ): Promise<Message | Message[]> {
+
+
+        const embed = new MessageEmbed()
+            .setColor(CONFIG.colours.yellow)
+            .setTitle("Guild Count:")
+            .setThumbnail(msg.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+            .setDescription(`I am serving **${msg.client.guilds.cache.size}** servers! :D`)
+            .setTimestamp();
+        return msg.channel.send(embed);
+    }
+}
