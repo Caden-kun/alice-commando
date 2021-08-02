d61c9ea11db0a3f2236a164587141fc95ebca879
Caden-kun <cadenripepi012@gmail.com>
2021-08-02 03:04:04 +0200

    i am legit an idiot

--- src/commands/whitelist/users.ts
+++ src/commands/whitelist/users.ts
@@ -38,7 +38,7 @@ export default class MemberCountCommand extends commando.Command {
             .setColor(CONFIG.colours.yellow)
             .setTitle("Guild Count:")
             .setThumbnail(msg.author.displayAvatarURL({ dynamic: true, size: 1024 }))
-            .setDescription(`I am serving **${msg.client.users.cache.size}** servers! :D`)
+            .setDescription(`I am serving **${msg.client.users.cache.size}** users!`)
             .setTimestamp();
         return msg.channel.send(embed);
     }
