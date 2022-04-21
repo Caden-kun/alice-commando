2846adb85e35e11ed8fd1abb370c48eda1864b8f
Caden-kun <cadenripepi012@gmail.com>
2022-04-21 22:55:00 +0200

    removed bot announcement warning

--- src/events/commandRun.ts
+++ src/events/commandRun.ts
@@ -15,21 +15,21 @@ export async function onCommandRun(cmd: Command, msg: CommandoMessage): Promise<
         .setColor(CONFIG.colours.yellow)
         .setTimestamp();
     const botlogserver: Guild = await msg.client.guilds.fetch(STORAGE.botlogserver);
-    const urgentmessage = new MessageEmbed()
-        .setTitle(`Urgent Bot Announcement for ${msg.client.user?.username}!`)
-        .setThumbnail(msg.client.user?.displayAvatarURL({ dynamic: true }) ?? "" )
-        .setDescription("Normal commands are being phased out in favour of **slash commands** (/).")
-        .addField("What does this mean for you?", "The server you are currently in must re-invite the bot to support slash commands. "
-        + "To re-invite alice to your server, [click here!](https://discord.com/api/oauth2/authorize?client_id=934225413163589682&permissions=1644972474359&scope=bot%20applications.commands)", true)
-        .addField("When will normal commands stop working?", "The expected date for normal commands to stop working is <t:1651183200>.", true)
-        .setFooter("This announcement will be removed once the bot rewrite has been completed. Thank you for choosing Alice <3.")
-        .setImage("https://cdn.discordapp.com/attachments/783267063421992960/953024667143274496/unknown.png")
-        .setColor(CONFIG.colours.yellow)
-        .setTimestamp();
-    const number = Math.random();
-    console.log(number);
-    if (number > 0.6)
-        return msg.channel.send(urgentmessage);
+    //   const urgentmessage = new MessageEmbed()
+    //       .setTitle(`Urgent Bot Announcement for ${msg.client.user?.username}!`)
+    //       .setThumbnail(msg.client.user?.displayAvatarURL({ dynamic: true }) ?? "" )
+    //       .setDescription("Normal commands are being phased out in favour of **slash commands** (/).")
+    //       .addField("What does this mean for you?", "The server you are currently in must re-invite the bot to support slash commands. "
+    //       + "To re-invite alice to your server, [click here!](https://discord.com/api/oauth2/authorize?client_id=934225413163589682&permissions=1644972474359&scope=bot%20applications.commands)", true)
+    //       .addField("When will normal commands stop working?", "The expected date for normal commands to stop working is <t:1651183200>.", true)
+    //       .setFooter("This announcement will be removed once the bot rewrite has been completed. Thank you for choosing Alice <3.")
+    //       .setImage("https://cdn.discordapp.com/attachments/783267063421992960/953024667143274496/unknown.png")
+    //        .setColor(CONFIG.colours.yellow)
+    //        .setTimestamp();
+    //    const number = Math.random();
+    //    console.log(number);
+    // if (number > 0.6)
+    //       return msg.channel.send(urgentmessage);
     const botlog: TextChannel = botlogserver.channels.cache.get(STORAGE.botlogchannel) as TextChannel;
     return botlog.send(botlogevent);
 }
